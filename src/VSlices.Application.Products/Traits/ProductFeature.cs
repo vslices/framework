3bdaebd151c216373cca06eabb05cf4423c405f8
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2026-08-26 17:20:22 -0400

    feat: update

--- src/VSlices.Application.Products/Traits/ProductFeature.cs
+++ src/VSlices.Application.Products/Traits/ProductFeature.cs
@@ -2,11 +2,27 @@
 
 namespace VSlices.Application;
 
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="F"></typeparam>
+/// <typeparam name="RT"></typeparam>
+/// <typeparam name="REQ"></typeparam>
+/// <typeparam name="RES"></typeparam>
 public interface ProductFeature<F, RT, REQ, RES> : Feature<F, RT, REQ, RES>
     where F : ProductFeature<F, RT, REQ, RES>
 {
+    /// <summary>
+    /// 
+    /// </summary>
     static abstract ProductRole ExecutableBy { get; }
 }
 
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="F"></typeparam>
+/// <typeparam name="RT"></typeparam>
+/// <typeparam name="REQ"></typeparam>
 public interface ProductFeature<F, RT, REQ> : ProductFeature<F, RT, REQ, Unit>
     where F : ProductFeature<F, RT, REQ>;
