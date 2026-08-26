02ccb4340faefd7eb491065953b3cb836add6db4
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2026-08-26 00:18:05 -0400

    feat: updated DbContextIO

---
+++ src/VSlices.EntityFrameworkCore/DbContextIO.Definition.cs
@@ -0,0 +1,15 @@
+﻿using LanguageExt;
+using LanguageExt.Traits;
+using Microsoft.EntityFrameworkCore;
+
+namespace VSlices.EntityFrameworkCore;
+
+public interface HasDbContext<RT, TContext> :
+    Has<Eff<RT>, DbContextIO<TContext>>
+    where TContext : DbContext;
+
+public interface DbContextIO<TContext>
+    where TContext : DbContext
+{
+    IO<TContext> Context { get; }
+}
