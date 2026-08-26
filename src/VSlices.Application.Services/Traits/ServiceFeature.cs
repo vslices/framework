6afd47678c77de48523b5831d4777cdee99a4a49
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-08-26 17:11:19 -0400

    fix: align service features with runtime-bound features

--- src/VSlices.Application.Services/Traits/ServiceFeature.cs
+++ src/VSlices.Application.Services/Traits/ServiceFeature.cs
@@ -2,26 +2,11 @@
 
 namespace VSlices.Application;
 
-/// <summary>
-/// 
-/// </summary>
-/// <typeparam name="F"></typeparam>
-/// <typeparam name="RT"></typeparam>
-/// <typeparam name="REQ"></typeparam>
-/// <typeparam name="RES"></typeparam>
-public interface ServiceFeature<F, REQ, RES> : Feature<F, REQ, RES>
-    where F : ServiceFeature<F, REQ, RES>
+public interface ServiceFeature<F, RT, REQ, RES> : Feature<F, RT, REQ, RES>
+    where F : ServiceFeature<F, RT, REQ, RES>
 {
-    /// <summary>
-    /// 
-    /// </summary>
     static abstract ServiceClaim ExecutableBy { get; }
 }
 
-/// <summary>
-/// 
-/// </summary>
-/// <typeparam name="F"></typeparam>
-/// <typeparam name="REQ"></typeparam>
-public interface ServiceFeature<F, REQ> : ServiceFeature<F, REQ, Unit>
-    where F : ServiceFeature<F, REQ>;
+public interface ServiceFeature<F, RT, REQ> : ServiceFeature<F, RT, REQ, Unit>
+    where F : ServiceFeature<F, RT, REQ>;
