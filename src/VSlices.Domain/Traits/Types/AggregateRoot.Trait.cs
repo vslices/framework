5d3de40493dfdbab230b4f607f18ad958b126569
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2026-08-27 02:38:46 -0400

    Agregados traits de aggregate root y entity

---
+++ src/VSlices.Domain/Traits/Types/AggregateRoot.Trait.cs
@@ -0,0 +1,13 @@
+﻿namespace VSlices.Domain.Traits;
+
+public interface AggregateRoot<SELF> : Entity<SELF>
+    where SELF : AggregateRoot<SELF>;
+
+public interface AggregateRoot<SELF, ID> : AggregateRoot<SELF>, Entity<SELF, ID>
+    where SELF : AggregateRoot<SELF, ID>
+    where ID : Identifier<ID>;
+
+public interface AggregateRoot<SELF, ID, REPR> : AggregateRoot<SELF, ID>, Entity<SELF, ID, REPR>
+    where SELF : AggregateRoot<SELF, ID, REPR>
+    where ID : Identifier<ID>;
+
