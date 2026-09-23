fd810058868436b021321d4429c77966b8d4a0f4
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-23 14:25:29 -0300

    Add fused UpdateTodo grounding

---
+++ examples/SampleWorkflow/SampleWorkflow.Grounding/OptimizedUpdateTodoWork.cs
@@ -0,0 +1,80 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using LanguageExt;
+using LanguageExt.Common;
+using LanguageExt.Traits;
+using SampleWorkflow.Spaces;
+using SampleWorkflow.Work;
+using VSlices.Work;
+using static LanguageExt.Prelude;
+
+namespace SampleWorkflow.Grounding;
+
+/// <summary>
+/// Fuses the UpdateTodoOptimized Line into one lookup, one semantic evolution,
+/// and one in-place dictionary value replacement.
+/// </summary>
+public sealed class OptimizedUpdateTodoWork :
+    AlgebraIO<UpdateTodoOptimized.Algebra>
+{
+    private readonly Dictionary<Guid, Todo> points = new();
+
+    public int Lookups { get; private set; }
+    public int Evolutions { get; private set; }
+    public int Writes { get; private set; }
+
+    public void Seed(Todo point) =>
+        points[point.Id.Value] = point;
+
+    public Option<Todo> Current(TodoId id) =>
+        points.TryGetValue(id.Value, out var point)
+            ? Some(point)
+            : None;
+
+    public IO<A> Interpret<A>(
+        K<UpdateTodoOptimized.Algebra, A> operation) =>
+        operation switch
+        {
+            UpdateTodoOptimized.ExecuteLinePart<A> execute =>
+                IO.lift(() => execute.Next(Execute(execute))),
+            _ => throw new NotSupportedException()
+        };
+
+    private Either<Error, Option<Todo>> Execute<A>(
+        UpdateTodoOptimized.ExecuteLinePart<A> execute)
+    {
+        var step =
+            execute.Line
+                .Process
+                .Flow
+                .Step;
+
+        Lookups++;
+
+        ref var current =
+            ref CollectionsMarshal.GetValueRefOrNullRef(
+                points,
+                step.Id.Value);
+
+        if (Unsafe.IsNullRef(ref current))
+        {
+            return Either.Right<Error, Option<Todo>>(
+                Option<Todo>.None);
+        }
+
+        Evolutions++;
+
+        return execute.Evolve(current)
+            .Match<Either<Error, Option<Todo>>>(
+                Succ: updated =>
+                {
+                    current = updated;
+                    Writes++;
+
+                    return Either.Right<Error, Option<Todo>>(
+                        Some(updated));
+                },
+                Fail: error =>
+                    Either.Left<Error, Option<Todo>>(error));
+    }
+}
