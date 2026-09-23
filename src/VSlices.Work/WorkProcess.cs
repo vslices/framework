087905df181a4c5b8c0ef1fb5aa1f65063a6d7cc
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-23 09:57:17 -0300

    Remove redundant WorkProcess abstraction

--- src/VSlices.Work/WorkProcess.cs
+++
@@ -1,8 +0,0 @@
-namespace VSlices.Work;
-
-public interface WorkProcess<P, ALG, REQ, RES>
-    where ALG : Functor<ALG>
-    where P : WorkProcess<P, ALG, REQ, RES>
-{
-    static abstract Free<ALG, RES> Get(REQ request);
-}
