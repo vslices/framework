71fc6566f84d4f795b716746ab51b6894c5ed156
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-26 08:03:48 -0300

    Model Flow runtimes as executable algebras

--- src/VSlices.Work/Algebra/AlgebraIO.cs
+++ src/VSlices.Work/Algebra/AlgebraIO.cs
@@ -1,58 +1,10 @@
 namespace VSlices.Work;
 
+/// <summary>
+/// Exposes a concrete executable algebra from Grounding.
+/// </summary>
+/// <typeparam name="ALG">The Work algebra being realized.</typeparam>
 public interface AlgebraIO<ALG>
-    where ALG : Functor<ALG>
 {
-    IO<A> Interpret<A>(K<ALG, A> operation);
-}
-
-public interface HasAlgebra<ALG, RT> : Has<Eff<RT>, AlgebraIO<ALG>>
-    where ALG : Functor<ALG>;
-
-public static class FreeAlgebra
-{
-    public static IO<A> interpret<ALG, A>(
-        K<Free<ALG>, A> program,
-        AlgebraIO<ALG> interpreter)
-        where ALG : Functor<ALG> =>
-        (Free<ALG, A>)program switch
-        {
-            Pure<ALG, A>(var value) =>
-                IO.pure(value),
-
-            Bind<ALG, A>(var operation) =>
-                interpreter
-                    .Interpret(operation)
-                    .Bind(next => interpret<ALG, A>(next, interpreter)),
-
-            _ => throw new NotSupportedException()
-        };
-
-    public static Free<G, A> hoist<N, F, G, A>(Free<F, A> program)
-        where N : Natural<F, G>
-        where F : Functor<F>
-        where G : Functor<G> =>
-        program switch
-        {
-            Pure<F, A>(var value) =>
-                Free.pure<G, A>(value),
-
-            Bind<F, A>(var operation) =>
-                Free.bind<G, A>(
-                    N.Transform(operation)
-                        .Map(next => hoist<N, F, G, A>(next))),
-
-            _ => throw new NotSupportedException()
-        };
-}
-
-public static class AlgebraEnv<ALG, RT>
-    where ALG : Functor<ALG>
-    where RT : HasAlgebra<ALG, RT>
-{
-    private static Eff<RT, AlgebraIO<ALG>> accessIO =>
-        Has<Eff<RT>, RT, AlgebraIO<ALG>>.ask.As();
-
-    public static Eff<RT, A> run<A>(K<Free<ALG>, A> program) =>
-        accessIO.Bind(interpreter => FreeAlgebra.interpret(program, interpreter));
+    ALG Algebra { get; }
 }
