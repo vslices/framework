b8790d4653adb4968a40ed0919b60a8f5c07e4af
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-26 08:05:31 -0300

    Use executable File algebra as Flow runtime

---
+++ examples/SampleFileRepo/SampleFileRepo/FileAlgebra.cs
@@ -0,0 +1,13 @@
+using VSlices.Work;
+
+namespace SampleFileRepo;
+
+public interface FileIdSource
+{
+    IO<SampleFileId> Next();
+}
+
+public sealed record FileAlgebra(
+    PointReader<SampleFile, SampleFileId> Reader,
+    PointWriter<SampleFile> Writer,
+    FileIdSource Ids);
