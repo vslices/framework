e51599bc4fa93a1f969a778410465dea01aec4b7
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-26 08:06:17 -0300

    Run examples directly on executable algebras

--- examples/SampleWorkflow/SampleWorkflow.Process.Tests/ProcessHoistTests.cs
+++ examples/SampleWorkflow/SampleWorkflow.Process.Tests/ProcessHoistTests.cs
@@ -1,57 +1,37 @@
-using LanguageExt;
-using LanguageExt.Traits;
 using SampleWorkflow.Grounding;
 using SampleWorkflow.Process;
 using SampleWorkflow.Spaces;
-using SampleWorkflow.Work;
-using VSlices.Work;
 using Xunit;
-using static LanguageExt.Prelude;
 
 namespace SampleWorkflow.Process.Tests;
 
 public sealed class ComposedFeatureFlowTests
 {
     [Fact]
-    public async Task Feature_composes_child_Features_through_Flow()
+    public async Task Feature_composes_child_Features_over_the_same_algebra()
     {
         var detail = TodoDetail.Transformation
             .RunFin("composed through flow")
             .ThrowIfFail();
 
-        var service = new InMemoryTodoWork();
-        var runtime = new ProcessRuntime(service);
+        var grounding = new InMemoryTodoWork();
 
-        var response = await CreateAndGetTodo<ProcessRuntime>
+        var response = await CreateAndGetTodo
             .Get()
             .RunFlow(
-                runtime,
-                new CreateAndGetTodo<ProcessRuntime>.Request(
+                grounding.Algebra,
+                new CreateAndGetTodo.Request(
                     detail,
                     Completed: false))
             .RunAsync();
 
-        var todo = response.Todo
-            .Match(
-                Left: error => throw error.ToException(),
-                Right: maybe => maybe.IfNone(
-                    () => throw new InvalidOperationException(
-                        "Expected the Process to return the created Todo.")));
+        var todo = response.Todo.Match(
+            Left: error => throw error.ToException(),
+            Right: maybe => maybe.IfNone(
+                () => throw new InvalidOperationException(
+                    "Expected the Process to return the created Todo.")));
 
         Assert.Equal("composed through flow", todo.Detail.Value);
         Assert.False(todo.Completed);
     }
-}
-
-public sealed record ProcessRuntime(InMemoryTodoWork Work) :
-    HasAlgebra<CreateTodoAlgebra, ProcessRuntime>,
-    HasAlgebra<GetTodoAlgebra, ProcessRuntime>
-{
-    static K<Eff<ProcessRuntime>, AlgebraIO<CreateTodoAlgebra>>
-        Has<Eff<ProcessRuntime>, AlgebraIO<CreateTodoAlgebra>>.Ask { get; } =
-        liftEff<ProcessRuntime, AlgebraIO<CreateTodoAlgebra>>(rt => (AlgebraIO<CreateTodoAlgebra>)rt.Work);
-
-    static K<Eff<ProcessRuntime>, AlgebraIO<GetTodoAlgebra>>
-        Has<Eff<ProcessRuntime>, AlgebraIO<GetTodoAlgebra>>.Ask { get; } =
-        liftEff<ProcessRuntime, AlgebraIO<GetTodoAlgebra>>(rt => (AlgebraIO<GetTodoAlgebra>)rt.Work);
 }
