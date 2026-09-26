febc3a31af0563ed293cb29763dbf314a2db6754
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-26 08:06:58 -0300

    Validate executable algebra runtimes without Free

--- tests/VSlices.Work.Algebra.Tests/AlgebraSumArityTests.cs
+++ tests/VSlices.Work.Algebra.Tests/AlgebraSumArityTests.cs
@@ -1,5 +1,5 @@
-using LanguageExt;
-using LanguageExt.Traits;
+using VSlices;
+using VSlices.Monads;
 using VSlices.Work;
 using Xunit;
 
@@ -8,139 +8,44 @@ namespace VSlices.Work.Tests;
 public sealed class AlgebraSumArityTests
 {
     [Fact]
-    public async Task Seven_way_sum_hoists_and_interprets_A_through_G()
+    public async Task Seven_way_sum_projects_each_child_runtime_into_Flow()
     {
-        var interpreter =
-            new AlgebraSumIO<
-                ValueAlgebra<ATag>,
-                ValueAlgebra<BTag>,
-                ValueAlgebra<CTag>,
-                ValueAlgebra<DTag>,
-                ValueAlgebra<ETag>,
-                ValueAlgebra<FTag>,
-                ValueAlgebra<GTag>>(
-                    new ValueInterpreter<ATag>(),
-                    new ValueInterpreter<BTag>(),
-                    new ValueInterpreter<CTag>(),
-                    new ValueInterpreter<DTag>(),
-                    new ValueInterpreter<ETag>(),
-                    new ValueInterpreter<FTag>(),
-                    new ValueInterpreter<GTag>());
+        var algebra = new AlgebraSum<
+            ValueAlgebra<ATag>,
+            ValueAlgebra<BTag>,
+            ValueAlgebra<CTag>,
+            ValueAlgebra<DTag>,
+            ValueAlgebra<ETag>,
+            ValueAlgebra<FTag>,
+            ValueAlgebra<GTag>>(
+                new(1),
+                new(2),
+                new(3),
+                new(4),
+                new(5),
+                new(6),
+                new(7));
 
-        var program =
-            from a in AlgebraSum<
-                ValueAlgebra<ATag>,
-                ValueAlgebra<BTag>,
-                ValueAlgebra<CTag>,
-                ValueAlgebra<DTag>,
-                ValueAlgebra<ETag>,
-                ValueAlgebra<FTag>,
-                ValueAlgebra<GTag>>
-                .FromA(ValueAlgebra<ATag>.Value(1))
-            from b in AlgebraSum<
-                ValueAlgebra<ATag>,
-                ValueAlgebra<BTag>,
-                ValueAlgebra<CTag>,
-                ValueAlgebra<DTag>,
-                ValueAlgebra<ETag>,
-                ValueAlgebra<FTag>,
-                ValueAlgebra<GTag>>
-                .FromB(ValueAlgebra<BTag>.Value(2))
-            from c in AlgebraSum<
-                ValueAlgebra<ATag>,
-                ValueAlgebra<BTag>,
-                ValueAlgebra<CTag>,
-                ValueAlgebra<DTag>,
-                ValueAlgebra<ETag>,
-                ValueAlgebra<FTag>,
-                ValueAlgebra<GTag>>
-                .FromC(ValueAlgebra<CTag>.Value(3))
-            from d in AlgebraSum<
-                ValueAlgebra<ATag>,
-                ValueAlgebra<BTag>,
-                ValueAlgebra<CTag>,
-                ValueAlgebra<DTag>,
-                ValueAlgebra<ETag>,
-                ValueAlgebra<FTag>,
-                ValueAlgebra<GTag>>
-                .FromD(ValueAlgebra<DTag>.Value(4))
-            from e in AlgebraSum<
-                ValueAlgebra<ATag>,
-                ValueAlgebra<BTag>,
-                ValueAlgebra<CTag>,
-                ValueAlgebra<DTag>,
-                ValueAlgebra<ETag>,
-                ValueAlgebra<FTag>,
-                ValueAlgebra<GTag>>
-                .FromE(ValueAlgebra<ETag>.Value(5))
-            from f in AlgebraSum<
-                ValueAlgebra<ATag>,
-                ValueAlgebra<BTag>,
-                ValueAlgebra<CTag>,
-                ValueAlgebra<DTag>,
-                ValueAlgebra<ETag>,
-                ValueAlgebra<FTag>,
-                ValueAlgebra<GTag>>
-                .FromF(ValueAlgebra<FTag>.Value(6))
-            from g in AlgebraSum<
-                ValueAlgebra<ATag>,
-                ValueAlgebra<BTag>,
-                ValueAlgebra<CTag>,
-                ValueAlgebra<DTag>,
-                ValueAlgebra<ETag>,
-                ValueAlgebra<FTag>,
-                ValueAlgebra<GTag>>
-                .FromG(ValueAlgebra<GTag>.Value(7))
-            select a + b + c + d + e + f + g;
+        var flow =
+            ValueFlow<ATag>().MapRuntime<AlgebraSum<ValueAlgebra<ATag>, ValueAlgebra<BTag>, ValueAlgebra<CTag>, ValueAlgebra<DTag>, ValueAlgebra<ETag>, ValueAlgebra<FTag>, ValueAlgebra<GTag>>>(x => x.A)
+            .Bind(a => ValueFlow<BTag>().MapRuntime<AlgebraSum<ValueAlgebra<ATag>, ValueAlgebra<BTag>, ValueAlgebra<CTag>, ValueAlgebra<DTag>, ValueAlgebra<ETag>, ValueAlgebra<FTag>, ValueAlgebra<GTag>>>(x => x.B)
+            .Bind(b => ValueFlow<CTag>().MapRuntime<AlgebraSum<ValueAlgebra<ATag>, ValueAlgebra<BTag>, ValueAlgebra<CTag>, ValueAlgebra<DTag>, ValueAlgebra<ETag>, ValueAlgebra<FTag>, ValueAlgebra<GTag>>>(x => x.C)
+            .Bind(c => ValueFlow<DTag>().MapRuntime<AlgebraSum<ValueAlgebra<ATag>, ValueAlgebra<BTag>, ValueAlgebra<CTag>, ValueAlgebra<DTag>, ValueAlgebra<ETag>, ValueAlgebra<FTag>, ValueAlgebra<GTag>>>(x => x.D)
+            .Bind(d => ValueFlow<ETag>().MapRuntime<AlgebraSum<ValueAlgebra<ATag>, ValueAlgebra<BTag>, ValueAlgebra<CTag>, ValueAlgebra<DTag>, ValueAlgebra<ETag>, ValueAlgebra<FTag>, ValueAlgebra<GTag>>>(x => x.E)
+            .Bind(e => ValueFlow<FTag>().MapRuntime<AlgebraSum<ValueAlgebra<ATag>, ValueAlgebra<BTag>, ValueAlgebra<CTag>, ValueAlgebra<DTag>, ValueAlgebra<ETag>, ValueAlgebra<FTag>, ValueAlgebra<GTag>>>(x => x.F)
+            .Bind(f => ValueFlow<GTag>().MapRuntime<AlgebraSum<ValueAlgebra<ATag>, ValueAlgebra<BTag>, ValueAlgebra<CTag>, ValueAlgebra<DTag>, ValueAlgebra<ETag>, ValueAlgebra<FTag>, ValueAlgebra<GTag>>>(x => x.G)
+                .Map(g => a + b + c + d + e + f + g)))))));
 
-        var result = await FreeAlgebra
-            .interpret(program, interpreter)
-            .RunAsync();
+        var result = await flow.RunFlow(algebra, default(Unit)).RunAsync();
 
         Assert.Equal(28, result);
     }
+
+    private static Flow<ValueAlgebra<TTag>, Unit, int> ValueFlow<TTag>() =>
+        new((algebra, _) => IO.pure(algebra.Value));
 }
 
-public sealed record ValueOperation<TTag, T>(
-    int Value,
-    Func<int, T> Next)
-    : K<ValueAlgebra<TTag>, T>;
-
-public sealed class ValueAlgebra<TTag> :
-    Functor<ValueAlgebra<TTag>>
-{
-    public static Free<ValueAlgebra<TTag>, int> Value(int value) =>
-        Free.lift<ValueAlgebra<TTag>, int>(
-            new ValueOperation<TTag, int>(
-                value,
-                static current => current));
-
-    static K<ValueAlgebra<TTag>, Y>
-        Functor<ValueAlgebra<TTag>>.Map<X, Y>(
-            Func<X, Y> f,
-            K<ValueAlgebra<TTag>, X> ma) =>
-        ma switch
-        {
-            ValueOperation<TTag, X>(var value, var next) =>
-                new ValueOperation<TTag, Y>(
-                    value,
-                    current => f(next(current))),
-            _ => throw new NotSupportedException()
-        };
-}
-
-public sealed class ValueInterpreter<TTag> :
-    AlgebraIO<ValueAlgebra<TTag>>
-{
-    public IO<T> Interpret<T>(
-        K<ValueAlgebra<TTag>, T> operation) =>
-        operation switch
-        {
-            ValueOperation<TTag, T>(var value, var next) =>
-                IO.pure(next(value)),
-            _ => throw new NotSupportedException()
-        };
-}
+public sealed record ValueAlgebra<TTag>(int Value);
 
 public sealed class ATag;
 public sealed class BTag;
