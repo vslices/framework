328fa8d00586118511ee03af6f83200f2309bc74
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-26 08:05:18 -0300

    Ground executable Todo algebra directly

--- examples/SampleWorkflow/SampleWorkflow.Grounding/InMemoryTodoWork.cs
+++ examples/SampleWorkflow/SampleWorkflow.Grounding/InMemoryTodoWork.cs
@@ -1,6 +1,5 @@
 using System.Collections.Concurrent;
 using LanguageExt;
-using LanguageExt.Traits;
 using SampleWorkflow.Spaces;
 using SampleWorkflow.Work;
 using VSlices.Work;
@@ -9,92 +8,49 @@ using static LanguageExt.Prelude;
 namespace SampleWorkflow.Grounding;
 
 public sealed class InMemoryTodoWork :
-    AlgebraIO<CreateTodoAlgebra>,
-    AlgebraIO<GetTodoAlgebra>,
-    AlgebraIO<UpdateTodoAlgebra>,
-    AlgebraIO<DeleteTodoAlgebra>,
-    AlgebraIO<AddAttachmentReferenceAlgebra>
+    AlgebraIO<TodoAlgebra>,
+    PointReader<Todo, TodoId>,
+    PointWriter<Todo>,
+    PointRemover<Todo, TodoId>,
+    TodoIdSource
 {
     private readonly ConcurrentDictionary<TodoId, Todo> points = new();
 
-    IO<A> AlgebraIO<CreateTodoAlgebra>.Interpret<A>(
-        K<CreateTodoAlgebra, A> operation) =>
-        operation switch
-        {
-            CreateTodoNextIdPart<A> next =>
-                IO.lift(Guid.NewGuid)
-                    .Bind(value => TodoId.Transformation
-                        .RunFin(value)
-                        .Match(
-                            Succ: id => IO.pure(next.Next(id)),
-                            Fail: IO.fail<A>)),
-            CreateTodoReadPart<A> read =>
-                IO.lift(() => read.Next(Read(read.Id))),
-            CreateTodoWritePart<A> write =>
-                IO.lift(() =>
-                {
-                    points[write.Point.Id] = write.Point;
-                    return write.Next(unit);
-                }),
-            _ => throw new NotSupportedException()
-        };
+    public TodoAlgebra Algebra { get; }
 
-    IO<A> AlgebraIO<GetTodoAlgebra>.Interpret<A>(
-        K<GetTodoAlgebra, A> operation) =>
-        operation switch
-        {
-            GetTodoReadPart<A> read =>
-                IO.lift(() => read.Next(Read(read.Id))),
-            _ => throw new NotSupportedException()
-        };
+    public InMemoryTodoWork() =>
+        Algebra = new(
+            Reader: this,
+            Writer: this,
+            Remover: this,
+            Ids: this);
 
-    IO<A> AlgebraIO<UpdateTodoAlgebra>.Interpret<A>(
-        K<UpdateTodoAlgebra, A> operation) =>
-        operation switch
-        {
-            UpdateTodoReadPart<A> read =>
-                IO.lift(() => read.Next(Read(read.Id))),
-            UpdateTodoWritePart<A> write =>
-                IO.lift(() =>
-                {
-                    points[write.Point.Id] = write.Point;
-                    return write.Next(unit);
-                }),
-            _ => throw new NotSupportedException()
-        };
+    public IO<TodoId> Next() =>
+        IO.lift(Guid.NewGuid)
+            .Bind(value =>
+                TodoId.Transformation
+                    .RunFin(value)
+                    .Match(
+                        Succ: IO.pure,
+                        Fail: IO.fail<TodoId>));
 
-    IO<A> AlgebraIO<DeleteTodoAlgebra>.Interpret<A>(
-        K<DeleteTodoAlgebra, A> operation) =>
-        operation switch
-        {
-            DeleteTodoReadPart<A> read =>
-                IO.lift(() => read.Next(Read(read.Id))),
-            DeleteTodoRemovePart<A> remove =>
-                IO.lift(() =>
-                {
-                    points.TryRemove(remove.Id, out _);
-                    return remove.Next(unit);
-                }),
-            _ => throw new NotSupportedException()
-        };
+    public IO<Option<Todo>> Read(TodoId id) =>
+        IO.lift(() =>
+            points.TryGetValue(id, out var point)
+                ? Some(point)
+                : Option<Todo>.None);
 
-    IO<A> AlgebraIO<AddAttachmentReferenceAlgebra>.Interpret<A>(
-        K<AddAttachmentReferenceAlgebra, A> operation) =>
-        operation switch
+    public IO<Unit> Write(Todo point) =>
+        IO.lift(() =>
         {
-            AddAttachmentReferenceReadPart<A> read =>
-                IO.lift(() => read.Next(Read(read.Id))),
-            AddAttachmentReferenceWritePart<A> write =>
-                IO.lift(() =>
-                {
-                    points[write.Point.Id] = write.Point;
-                    return write.Next(unit);
-                }),
-            _ => throw new NotSupportedException()
-        };
+            points[point.Id] = point;
+            return unit;
+        });
 
-    private Option<Todo> Read(TodoId id) =>
-        points.TryGetValue(id, out var point)
-            ? Some(point)
-            : Option<Todo>.None;
+    public IO<Unit> Remove(TodoId id) =>
+        IO.lift(() =>
+        {
+            points.TryRemove(id, out _);
+            return unit;
+        });
 }
